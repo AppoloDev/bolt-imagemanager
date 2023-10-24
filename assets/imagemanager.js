346ec68f6c543080031260d1ca6f0a25e05e5173
Fredxd <frederic@appolo.fr>
2023-10-24 14:18:00 +0200

    fix remove btn & preview

--- assets/imagemanager.js
+++ assets/imagemanager.js
@@ -1,11 +1,12 @@
-import {FileManager} from "./FileManager.standalone.js";
-import "./FileManager.css";
+import {FileManager} from "filemanager-element";
+import "filemanager-element/FileManager.css";
 
 class ImageManager extends HTMLElement {
   connectedCallback() {
     this.filemanager = this.querySelector("file-manager");
     this.imageManagerButton = this.querySelector("button#image-manager");
     this.fileInput = this.querySelector('input[name$="[filename]"]');
+    this.altInput = this.querySelector('input[name$="[alt]"]');
 
     if (this.filemanager && this.imageManagerButton && this.fileInput) {
       FileManager.register();
@@ -16,6 +17,9 @@ class ImageManager extends HTMLElement {
 
       this.querySelector('button#remove').addEventListener('click', () => {
         this.changeValue('');
+        if (this.altInput) {
+          this.altInput.value = '';
+        }
       })
 
       this.filemanager.addEventListener("close", () => {
@@ -39,7 +43,7 @@ class ImageManager extends HTMLElement {
 
   changePreview(value) {
     if (value) {
-      this.querySelector('.image--preview').innerHTML = `<a href="${value}" class="editor__image--preview-image" style="background-image: url(${value});background-size: contain"><span class="sr-only">Preview the image</span></a>`
+      this.querySelector('.image--preview').innerHTML = `<a href="${value}" class="editor__image--preview-image" style="background-image: url('${value}');background-size: contain"><span class="sr-only">Preview the image</span></a>`
     } else {
       this.querySelector('.image--preview').innerHTML = '';
     }
